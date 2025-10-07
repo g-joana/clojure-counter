9f799ba3d526c5b91ec9738786d46d27dc5b2153
Joana <jou.42.rio@gmail.com>
2025-10-06 21:14:36 -0300

    fix cws connection

--- src/frontend/ws.cljs
+++ src/frontend/ws.cljs
@@ -1,8 +1,11 @@
 (ns frontend.ws)
 
+(defonce connection (atom nil))
+
 (defn update-counter! [new-value]
   (when-let [element (.getElementById js/document "counter")]
     (set! (.-innerHTML element) new-value)))
+
 
 (defn on-open [event]
   (println "ws connection established")
@@ -17,13 +20,18 @@
 
 (defn on-close [event]
   (println "ws connection closed")
-  (js/console.log "code:" (.-code event) "reason:" (.-reason event)))
+  (js/console.log "code:" (.-code event) "reason:" (.-reason event))
+  (reset! connection nil))
 
 (defn connect! [url]
+  (if @connection
+    (println (str "connected to" url))
     (let [ws (js/WebSocket. url)]
       (set! (.-onopen ws) on-open)
       (set! (.-onmessage ws) on-message)
       (set! (.-onerror ws) on-error)
-      (set! (.-onclose ws) on-close)))
+      (set! (.-onclose ws) on-close)
+      (reset! connection ws)
+      (println (str "connecting to " url "...")))))
 
-(defonce connection (connect! "http://localhost:8080/ws"))
+(connect! "http://localhost:8080/ws")
